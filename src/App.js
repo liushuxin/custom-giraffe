d1fefba250b5321d1307830603f209b7f4fbad07
user <user@users-MacBook-Pro.local>
2022-01-16 16:04:19 +0800

    move api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import Expenses from './routes/expenses';
-import Invoices from './routes/invoices';
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -20,21 +20,25 @@ function App() {
     <main>
       <h1>Create React App + Go API</h1>
       <h2>
-        Deployed with{' '}
-        <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
+        Deployed with{" "}
+        <a
+          href="https://vercel.com/docs"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           Vercel
         </a>
         !
       </h2>
       <nav
         style={{
-          borderBottom: 'solid 1px',
-          paddingBottom: '1rem',
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
         }}
       >
-        <Link to="/invoices">Invoices</Link> | <Link to="/expenses">Expenses</Link>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
         <Routes>
-          <Route path="/" element={<App />} />
           <Route path="expenses" element={<Expenses />} />
           <Route path="invoices" element={<Invoices />} />
         </Routes>
